e8c5fc483bfde7389d3aa9c3b54f2fe3d07984ff
crystaldaking <crystal@crystalapps.by>
2020-12-11 11:30:25 +0300

    Define basic roles

--- src/Client.php
+++ src/Client.php
@@ -2,5 +2,32 @@
 namespace CrystalApps\Mt5Connector;
 class Client
 {
+    private $connection = null;
+    private $ip = null;
+    private $port = null;
+    private $login = null;
+    private $password = null;
+    private $agent = null;
+    private $httpClient = null;
 
+    public function __construct($connection,$ip,$port,$login,$password,$agent)
+    {
+        $this->connection = $connection;
+        $this->ip = $ip;
+        $this->port = $port;
+        $this->login = $login;
+        $this->password = $password;
+        $this->agent = $agent;
+    }
+
+    public function Connect()
+    {
+        $this->httpClient = new \GuzzleHttp\Client(
+            [
+                'base_uri' => $this->ip.':'.$this->port,
+                'timeout' => 36000,
+                'verify' => false,
+            ]
+        );
+    }
 }
